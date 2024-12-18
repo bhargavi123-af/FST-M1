fa719715e36dff0d9ed747828728945676b0d6b7
bhargavi123-af <bhargavig2020js@gmail.com>
2024-12-18 11:19:55 +0530

    Add files via upload

---
+++ Java/Activities/HelloWorld.java
@@ -0,0 +1,37 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		//String name= "Gunipalli Bhargavi";
+		//System.out.println(name);
+		//System.out.println(name.toUpperCase());
+		//System.out.println(name.toLowerCase());
+		
+//		String firstName = "Gunipalli";
+//		String lastName = "Bhargavi";
+//		System.out.println(firstName + " " + lastName);
+		
+//		System.out.println("Hello World");
+		
+//		int i = 0;
+//		while (i < 5) {
+//		    System.out.println(i);
+//		    i++;
+//		}
+		
+		
+		int a = 20;
+		int b = 18;
+		if (a > b) {
+		  System.out.println("a is greater than b");
+		}
+		else
+		{
+			System.out.println("b is greater than a");
+		}
+	
+			
+	}
+	
+
+}
