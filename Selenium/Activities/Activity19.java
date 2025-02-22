b6cbdbcaf6bab39c45bd3d6c4343f264dbea9df7
bhargavi123-af <bhargavig2020js@gmail.com>
2025-02-22 12:54:32 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity19.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity19 {
+	
+	public static void main(String[] args) {
+        
+        WebDriver driver = new FirefoxDriver();
+
+        
+        driver.get("https://training-support.net/webelements/alerts");
+        
+        System.out.println("Page title: " + driver.getTitle());
+
+        
+        driver.findElement(By.id("confirm")).click();
+
+       
+        Alert confirmAlert = driver.switchTo().alert();
+
+        
+        String alertText = confirmAlert.getText();
+        System.out.println("Text in alert: " + alertText);
+
+        
+        confirmAlert.accept();
+
+        
+        System.out.println(driver.findElement(By.id("result")).getText());
+
+        // Close the browser
+        driver.quit();
+    }
+
+}
